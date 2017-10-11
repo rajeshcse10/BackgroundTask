9824860bb927dd4992ebf4beaf8076055825cc35
MacBook Retina <Rajesh@MacBooks-MacBook-Pro.local>
2017-10-11 21:12:50 +0600

    Initial Commit

---
+++ MyBackgroundTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBackgroundTask
+//
+//  Created by MacBook Pro Retina on 10/11/17.
+//  Copyright © 2017 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBackgroundTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBackgroundTask
+//
+//  Created by MacBook Pro Retina on 10/11/17.
+//  Copyright © 2017 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
